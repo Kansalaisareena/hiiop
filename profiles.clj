e6cf20aeb52cfeb486defa4bc2ae6d9b9a1b8cd1
Tuukka Wahtera <tuukka.wahtera@futurice.com>
2016-11-03 13:47:15 +0200

    Remove and ignore profiles.clj

--- profiles.clj
+++
@@ -1,6 +0,0 @@
-;; WARNING
-;; The profiles.clj file is used for local environment variables, such as database credentials.
-;; This file is listed in .gitignore and will be excluded from version control by Git.
-
-{:profiles/dev  {:env {:database-url "postgresql://localhost/hiiop_dev?user=db_user_name_here&password=db_user_password_here"}}
- :profiles/test {:env {:database-url "postgresql://localhost/hiiop_test?user=db_user_name_here&password=db_user_password_here"}}}
