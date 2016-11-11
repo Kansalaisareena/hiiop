39f4a6fb3d462ca7e3483fe2a12738ea43874d44
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2016-11-11 13:53:21 +0100

    Change /api/config to /api/v1/config
    ...and fix client-config handling

--- src/clj/hiiop/routes/services.clj
+++ src/clj/hiiop/routes/services.clj
@@ -12,17 +12,24 @@
                            :title "Sample API"
                            :description "Sample Services"}}}}
   (context "/api" []
-    :tags ["thingie"]
+    :tags ["API"]
 
-    (GET "/config" []
-      (ok (select-keys env [:dev :lang])))
-    (POST "/login" []
-      :query-params [email :- String, password :- String]
-      :summary "Tries to log the user in. Returns
+    (context "/v1" []
+      :tags ["V1"]
+
+      (GET "/config" []
+        (ok (select-keys env [:dev :lang :git-ref])))
+
+      (POST "/login" []
+        :query-params [email :- String, password :- String]
+        :summary "Tries to log the user in. Returns
                  true/false on success/failure"
-      api-handlers/login-handler)
-    (GET "/login-status" [] api-handlers/login-status)
-    (GET "/show-session" [] api-handlers/show-session)
-    (GET "/logout" []
-      :summary "Logs the user out."
-      api-handlers/logout)))
+        api-handlers/login-handler)
+
+      (GET "/login-status" [] api-handlers/login-status)
+
+      (GET "/show-session" [] api-handlers/show-session)
+
+      (GET "/logout" []
+        :summary "Logs the user out."
+        api-handlers/logout))))
