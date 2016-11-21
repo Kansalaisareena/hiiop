0f0fab9903a2b379d4d9423e21f5757aa3c04fca
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2016-11-21 17:12:44 +0200

    Change logging to timbre

--- env/dev/clj/hiiop/env.clj
+++ env/dev/clj/hiiop/env.clj
@@ -1,6 +1,6 @@
 (ns hiiop.env
   (:require [selmer.parser :as parser]
-            [clojure.tools.logging :as log]
+            [taoensso.timbre :as log]
             [hiiop.dev-middleware :refer [wrap-dev]]))
 
 (def defaults
