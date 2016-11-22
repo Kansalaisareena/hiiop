0da5b11cc1d218d759165ba490a024ca535ce282
Tuukka Wahtera <tuukka.wahtera@futurice.com>
2016-11-22 17:54:46 +0200

    Rebase onto master

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -25,8 +25,9 @@ UPDATE users
 SET name = :name, email = :email
 WHERE id = :id
 
--- :name get-user :? :email
--- :doc retrieve a user given the email.
+
+-- :name get-user :? :1 :uuid
+-- :doc retrieve a user given the uuid.
 SELECT
   id,
   name,
@@ -70,25 +71,20 @@ INSERT INTO events
   :start_time,
   :end_time)
 
--- :name add! :! :n
--- :doc sln
-INSERT INTO test (time)
-VALUES (:time)
-
--- :name add-test-user! :! :n
--- :doc sln
-INSERT INTO users (email, pass)
-VALUES (:email, :pass)
-
-
--- :name sln-get :? :*
--- :doc get all times
-SELECT * FROM test
+-- :name add-full-user! :? :1
+-- :doc "add a new registered user"
+INSERT INTO users (id, email, name, pass)
+VALUES (DEFAULT, :email, :name, :pass )
+ON CONFLICT (email) DO
+  UPDATE
+    SET pass = :pass
+    WHERE users.name IS NULL
+RETURNING id
 
 -- :name delete-user! :? :*
 -- :doc delete user by email
 DELETE FROM users
-where email=:email
+where email = :email
 
 -- :name get-users :? :*
 -- :doc get all users
@@ -98,3 +94,5 @@ SELECT * from users
 -- :doc get user id by email
 SELECT id FROM users
 WHERE email = :email
+
+  
