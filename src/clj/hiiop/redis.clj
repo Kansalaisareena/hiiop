494673727e67dd89cebc575a3ba0e26c1542aa97
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2016-12-14 16:49:20 +0200

    Fix redis

--- src/clj/hiiop/redis.clj
+++ src/clj/hiiop/redis.clj
@@ -1,5 +1,20 @@
 (ns hiiop.redis
-  (:require [taoensso.carmine :as car]
-            [hiiop.config :refer [env]]))
+  (:require [mount.core :refer [defstate]]
+            [taoensso.carmine :as car]
+            [taoensso.carmine.ring :refer [carmine-store]]
+            [taoensso.timbre :as log]
+            [hiiop.config :refer [env]])
+  (:import [java.net URI]))
 
-(defmacro wcar* [& body] `(car/wcar {:pool {} :spec (env :redis)} ~@body))
+(defstate redis-connection-options
+  :start (let [uri (env :redis)]
+           (log/info "redis connection" uri)
+           {:spec (env :redis)}))
+
+(defstate session-store
+  :start (carmine-store
+          redis-connection-options))
+
+(defmacro wcar*
+  [& body]
+  `(car/wcar (conj {:pool {}} redis-connection-options) ~@body))
