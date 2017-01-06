de3f02badbaf1f21f7910e98bd790770bda8a61c
Nathan Dao <nathan@guynathan.com>
2017-01-06 11:38:15 +0200

    fix update-quest query

--- src/clj/hiiop/api_handlers.clj
+++ src/clj/hiiop/api_handlers.clj
@@ -207,6 +207,10 @@
         (assoc :owner (:id user))
         (dissoc :organiser-participates)
         (hc/api-quest->db-quest-coercer)
+        ((fn [lol]
+           (log/info "----------------------LOLOLOLOLOLOL")
+           (log/info lol)
+           lol))
         (db/update-quest!)
         (#(db/get-unmoderated-quest-by-id {:id (:id %) :owner (:id user)}))
         (#(if %1
